470b32bc21b41ffdccdc02bd2a47abb97430683f
BeltonAlberto <bymenete@gmail.com>
2022-10-14 00:38:03 +0200

    #refactoring

--- app.js
+++ app.js
@@ -1,64 +1,60 @@
-// Selecting elements
+// Creating Players
 
-const p1button = document.querySelector('#p1-button'),
-    p2button = document.querySelector('#p2-button'),
-    resetButton = document.querySelector('#reset-button'),
-    p1Display = document.querySelector('.p1-display'),
-    p2Display = document.querySelector('.p2-display'),
+const p1 = {
+    score: 0,
+    button: document.querySelector('#p1-button'),
+    display: document.querySelector('.p1-display'),
+};
+
+const p2 = {
+    score: 0,
+    button: document.querySelector('#p2-button'),
+    display: document.querySelector('.p2-display'),
+};
+
+const resetButton = document.querySelector('#reset-button'),
     select = document.querySelector('.target-num');
 
-// The score section
-
-let p1Score = 0,
-    p2Score = 0,
-    winningScore = 0,
+let winningScore = 0,
     isGameOver = false;
 
-// player one Logic
+// events section
 
-p1button.addEventListener('click', () => {
-    if (!isGameOver) {
-        p1Score++;
-        console.log(select);
-        if (p1Score === winningScore) {
-            isGameOver = true;
-            p1Display.classList.add('has-text-success');
-            p2Display.classList.add('has-text-danger');
-        }
-        p1Display.textContent = p1Score;
-    }
+p1.button.addEventListener('click', () => {
+    updateScore(p1, p2);
 });
 
-// player two Logic
-
-p2button.addEventListener('click', () => {
-    if (!isGameOver) {
-        p2Score++;
-        if (p2Score === winningScore) {
-            isGameOver = true;
-            p2Display.classList.add('has-text-success');
-            p1Display.classList.add('has-text-danger');
-        }
-        p2Display.textContent = p2Score;
-    }
+p2.button.addEventListener('click', () => {
+    updateScore(p2, p1);
 });
-
-// choosing the the Winning number logic
-// here the arrow function syntax is not used here cause of the this keyword
 
 select.addEventListener('change', function () {
     winningScore = +this.value;
     reset();
 });
 
-// reseting logic
-
 resetButton.addEventListener('click', reset);
 
+// function keyword will be hoisted
+
+function updateScore(player, opponent) {
+    if (!isGameOver) {
+        player.score++;
+        console.log(select);
+        if (player.score === winningScore) {
+            isGameOver = true;
+            player.display.classList.add('has-text-success');
+            opponent.display.classList.add('has-text-danger');
+        }
+        player.display.textContent = player.score;
+    }
+}
+
 function reset() {
-    p1Score = p2Score = 0;
-    p1Display.textContent = p2Display.textContent = p1Score;
     isGameOver = false;
-    p1Display.classList.remove('has-text-success', 'has-text-danger');
-    p1Display.classList.remove('has-text-success', 'has-text-danger');
+    for (let player of [p1, p2]) {
+        player.score = 0;
+        player.display.textContent = player.score;
+        player.display.classList.remove('has-text-success', 'has-text-danger');
+    }
 }
